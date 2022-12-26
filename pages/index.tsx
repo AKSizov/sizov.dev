63633da6675a5897b37fcf45b20f108a7874388a
AKSizov <37084670+AKSizov@users.noreply.github.com>
2022-12-26 18:33:13 -0500

    blank slate

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Website Services | Sizov.dev</title>
+        <meta name="description" content="Create your next site with sizov.dev" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        
+      </main>
+    </>
+  )
+}
