be63333fa912a87090e0d44ed06248eee7bc4ebb
AKSizov <37084670+AKSizov@users.noreply.github.com>
2023-01-02 20:40:09 -0500

    feat: flowbite

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import 'tw-elements';
 import { Inter } from '@next/font/google'
+import { Dropdown } from "flowbite-react";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,10 +14,24 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <main className="dark:bg-zinc-900 min-h-screen dark:text-white">
+      <main className="dark:bg-zinc-900 min-h-screen dark:text-white">
         <h1 className="text-3xl font-bold underline">
           Hello, Next.js!
         </h1>
+        <Dropdown label="Dropdown button">
+          <Dropdown.Item>
+            Dashboard
+          </Dropdown.Item>
+          <Dropdown.Item>
+            Settings
+          </Dropdown.Item>
+          <Dropdown.Item>
+            Earnings
+          </Dropdown.Item>
+          <Dropdown.Item>
+            Sign out
+          </Dropdown.Item>
+        </Dropdown>
       </main>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
