bb9279ef61e7a947a3fb38ec94a72535e605ab3b
AKSizov <37084670+AKSizov@users.noreply.github.com>
2023-02-05 23:19:32 -0500

    new: add resume section

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import resheader from '../public/undraw_message_sent_re_q2kl.svg'
+import resheader from '../public/undraw_certificate_re_yadi.svg'
 import soonimg from '../public/pop-zebra-4q3Ogm3Kt44-unsplash.webp'
 import colabimg from '../public/undraw_developer_activity_re_39tg.svg'
 import { Inter } from '@next/font/google'
@@ -34,7 +34,7 @@ export default function Home() {
               <div className='h-0.5 w-32 bg-neutral-300 mt-4'></div>
               <p className='text-2xl mt-4 text-center lg:text-left'>I'm a full stack developer and system administrator who has been in the industry for  over 8+ years. I've developed software for startups, open source projects, and major corporations</p>
               <div className="flex flex-col sm:flex-row justify-center my-8 mt-6 gap-4 w-full sm:w-auto">
-                <a href='#4'>
+                <a href='#resume'>
                   <button className="text-white border-2 rounded border-violet-600 py-2 px-6 hover:bg-violet-600 text-lg text-center w-full sm:w-auto">View my resume -&gt;</button>
                 </a>
                 <a href='#2'>
@@ -290,7 +290,7 @@ export default function Home() {
 
         <section className="text-neutral-400 bg-neutral-900/0 backdrop-brightness-50 body-font flex flex-col py-4 px-6" id='3'>
           <div className='my-16 flex flex-col lg:mx-16'>
-            <h1 className='text-5xl text-white font-bold text-center my-2'>Projects</h1>
+            <h1 className='text-5xl text-white font-medium text-center my-2'>Projects</h1>
             <p className='text-xl text-center'>Take a look at what I'm working on, and what's coming next.</p>
           </div>
           <div className="flex flex-wrap text-center">
@@ -317,31 +317,17 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="text-neutral-400 bg-neutral-900/90 backdrop-brightness-50 body-font">
-          <div className="container px-5 py-8 mx-auto flex flex-wrap items-center">
-            <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
-              <h1 className="title-font font-medium text-3xl text-white">Custom services</h1>
-              <p className="leading-relaxed my-4">
-                If the packages above don't suit your needs, no worries! <br />
-                Just shoot me an email at s‌erv‌ices‌@si‌zov.dev and we can have a chat to discuss your individual project needs.
+        <section className="text-neutral-400 bg-neutral-900/90 backdrop-brightness-50 body-font" id='resume'>
+          <div className="px-6 py-8 mx-auto flex flex-col lg:flex-row justify-evenly items-center">
+            <Image className="w-72 h-72 m-8" alt="hero" src={resheader} />
+            <div className="flex flex-col justify-center">
+              <h1 className="font-medium text-5xl text-white text-center lg:text-left">Resume</h1>
+              <p className="text-xl my-4 text-center lg:text-left">
+                Download my resume to view my work history, full list of skills, and more!
               </p>
-              <a href='mailto:services@sizov.dev'>
-                <button className="inline-flex text-white bg-violet-500 border-0 py-2 px-6 focus:outline-none hover:bg-violet-600 rounded text-lg">Contact me at services@sizov.dev</button>
+              <a className='text-center lg:text-left' target="_blank" href='/sizov_resume.pdf'>
+                <button className="text-white border-2 rounded border-violet-600 py-2 px-6 text-lg text-center w-full sm:w-auto my-4">Open resume.pdf -&gt;</button>
               </a>
-            </div>
-            <div className="lg:w-2/6 md:w-1/2 bg-opacity-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-              <Image className="object-cover object-center rounded" alt="hero" src={colabimg} />
-              {/* <h2 className="text-white text-lg font-medium title-font mb-5">Sign Up</h2>
-              <div className="relative mb-4">
-                <label for="full-name" className="leading-7 text-sm text-neutral-400">Full Name</label>
-                <input type="text" id="full-name" name="full-name" className="w-full bg-neutral-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-violet-900 rounded border border-neutral-600 focus:border-violet-500 text-base outline-none text-neutral-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></input>
-              </div>
-              <div className="relative mb-4">
-                <label for="email" className="leading-7 text-sm text-neutral-400">Email</label>
-                <input type="email" id="email" name="email" className="w-full bg-neutral-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-violet-900 rounded border border-neutral-600 focus:border-violet-500 text-base outline-none text-neutral-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></input>
-              </div>
-              <button className="text-white bg-violet-500 border-0 py-2 px-8 focus:outline-none hover:bg-violet-600 rounded text-lg">Button</button>
-              <p className="text-xs mt-3">Literally you probably haven't heard of them jean shorts.</p> */}
             </div>
           </div>
         </section>
